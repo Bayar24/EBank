015a73fcc35f46ebee378a395c90c0bd2f64df93
bayar24 <kykla_bbox@yahoo.com>
2018-05-24 09:14:14 -0500

    Changes

--- EAdmin/src/app/account/account.component.ts
+++ EAdmin/src/app/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,21 +13,26 @@ export class AccountComponent implements OnInit {
   private sub: Subscription;
   private accounts: any;
   private custid: string;
-  displayedColumns = ['acct_no', 'cust_no', 'acct_name', 'current_bal',  'acct_status', 'bank_account'];
+  displayedColumns = ['acct_no', 'cust_no', 'acct_name', 'current_bal', 'acct_status', 'bank_account'];
+  dataSource = new MatTableDataSource(this.accounts);
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
   constructor(private route: ActivatedRoute, private data: DataService) { }
 
   ngOnInit() {
     this.custid = this.route.snapshot.paramMap.get('id');
     if (this.custid !== undefined && this.custid !== null) {
       this.sub = this.data.getCustAccounts(parseInt(this.custid)).subscribe(
-        res => {this.accounts = res; console.log(res)},
+        res => { this.accounts = res; this.dataSource = new MatTableDataSource(this.accounts); console.log(res) },
         err => console.log(err));
     }
-    else
-    {
+    else {
       this.sub = this.data.getAllAccounts().subscribe(
-        res => {this.accounts = res; console.log(res)},
+        res => { this.accounts = res; this.dataSource = new MatTableDataSource(this.accounts); console.log(res) },
         err => console.log(err));
     }
   }
